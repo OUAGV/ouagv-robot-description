85f311b0091c77fcfbb4cd1c5c068bf08b3af1b2
t-nishimura <hbvcg00@gmail.com>
2022-10-30 11:30:52 +0900

    lint

--- launch/show_urdf.launch.py
+++ launch/show_urdf.launch.py
@@ -5,31 +5,30 @@ import xacro
 from ament_index_python.packages import get_package_share_directory
 
 import launch
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import LaunchConfiguration
 
-share_dir_path = os.path.join(
-    get_package_share_directory('ouagv_robot_description'))
-urdf_path = os.path.join(share_dir_path, 'urdf', 'urdf_test.urdf')
-rviz_config_path = os.path.join(share_dir_path, 'rviz', 'show_urdf.rviz')
+share_dir_path = os.path.join(get_package_share_directory("ouagv_robot_description"))
+urdf_path = os.path.join(share_dir_path, "urdf", "urdf_test.urdf")
+rviz_config_path = os.path.join(share_dir_path, "rviz", "show_urdf.rviz")
 
 
 def generate_launch_description():
 
     doc = xacro.process_file(urdf_path)
-    robot_desc = doc.toprettyxml(indent='  ')
+    robot_desc = doc.toprettyxml(indent="  ")
 
-    rsp = launch_ros.actions.Node(package='robot_state_publisher',
-                                  executable='robot_state_publisher',
-                                  output='both',
-                                  arguments=[urdf_path],
-                                  parameters=[{'robot_description': robot_desc}])
+    rsp = launch_ros.actions.Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        arguments=[urdf_path],
+        parameters=[{"robot_description": robot_desc}],
+    )
 
     rviz = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        output='both',
-        arguments=['-d', [rviz_config_path]])
+        package="rviz2",
+        executable="rviz2",
+        output="both",
+        arguments=["-d", [rviz_config_path]],
+    )
 
     return launch.LaunchDescription([rsp, rviz])
